c2f128c063d5334cbe27abd166341a6e10d06f8f
madayari <dhaher.mohamedamin@gmail.com>
2025-05-23 15:19:06 +0100

    complete the game into

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -7,9 +7,14 @@ int main() {
     cout << "Enter your name: ";
     cin >> player_name;
 
-    cout << "Great Start " << player_name << ", looks like you have understood the main() function properly now!" << endl;
+    cout << "Great Start " << player_name << endl;
 
     cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
+    cout << "Today is a momentous day—you’ll be choosing your very first Pokémon.\n";
+
+    cout << "Every great Trainer remembers this moment for the rest of their lives.\n";
+
+    cout << "So, choose wisely, young one!\n";
     cout << "You can choose one of the following Pokémon:\n";
     cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
     cout << "Which Pokémon would you like to choose? Enter the number: ";
@@ -29,6 +34,11 @@ int main() {
     else {
         cout << "Invalid choice. Please restart the game.\n";
     }
-
+    cout << "Ah, an excellent choice!\n";
+    cout << "But beware, Trainer,\n";
+    cout << "this is only the beginning.\n";
+    cout << "Your journey is about to unfold.\n";
+    cout << "Now let’s see if you’ve got what it takes to keep going!\n";
+    cout << "Good luck, and remember… Choose wisely!\n";
     return 0;
 }
